1e2728075a57dcec47f2e416620e333fc4c834c8
Stefan <stefan@layereight.de>
2022-07-01 00:41:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# cmi-api-extractor
+
+Extract data from TA's  
+[Control and Monitoring Interface (C.M.I)](https://www.ta.co.at/x2-bedienung-schnittstellen/cmi/).
+The C.M.I. connects to a TA solar and heating control unit like the [UVR16x2](https://www.ta.co.at/x2-frei-programmierbare-regler/uvr16x2/)
+and logs certain data values from the control unit's various inputs, outputs and buses.
+The python script queries the [C.M.I.'s JSON API](https://wiki.ta.co.at/C.M.I._JSON-API) to retrieve data.
+
+The data from the JSON API are mapped into metrics of a time series database. Currently only exporting
+metrics to [Prometheus](https://prometheus.io/) via its [Pushgateway](https://github.com/prometheus/pushgateway/blob/master/README.md)
+is supported. JSON API to metrics mapping is done via a config file.
+
+A cron job can be used to periodically retrieve and update values.
+
+## Prerequisits
+
+* python3
+* install required python libraries
+  `pip install -r requirements.txt`
+* running and reachable C.M.I.
+* running and reachable Prometheus Pushgateway instance
+
+## Usage
+
+```shell
+$ ./cmi-api-extractor.py <cmi_hostname> <cmi_username> <cmi_password> <cmi_id> <pushgateway_host>
+```
+
+## Helpful Commands
+
+* delete metric from prometheus pushgateway
+  `curl -i -X DELETE http://<host>:9091/metrics/job/<job_name>`
+* delete metric from prometheus
+  `curl -X POST "http://<host>:9090/api/v1/admin/tsdb/delete_series?match[]=<metric_name>"`
+
+## Other Links
+
+* [Technische Alternative (TA) website](https://www.ta.co.at/)
+* [C.M.I. wiki entry](https://wiki.ta.co.at/C.M.I.)
+* https://github.com/prometheus/pushgateway/blob/master/README.md#exposed-metrics
+* https://prometheus.io/docs/practices/pushing/
+* https://github.com/prometheus/client_python
---
+++ cmi-api-extractor.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python3
+
+import sys
+import requests
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+
+if len(sys.argv) != 6:
+    print('Arguments not matching! Usage:')
+    print(sys.argv[0] + ' <cmi_host> <cmi_username> <cmi_password> <cmi_id> <pushgateway_host>')
+    sys.exit(1)
+
+cmi_host = sys.argv[1]
+cmi_username = sys.argv[2]
+cmi_password = sys.argv[3]
+cmi_id = sys.argv[4]
+pushgateway_host = sys.argv[5] + ':9091'
+
+metric_prefix = 'test_2_cmi'
+metric_mapping_config = {
+    "Logging Analog": {
+        "1": {
+            "metric": "collector_celsius"
+        },
+        "2": {
+            "metric": "reservoir_celsius",
+            "labels": {
+                "layer": "top 1"
+            }
+        },
+        "3": {
+            "metric": "reservoir_celsius",
+            "labels": {
+                "layer": "bottom 2"
+            }
+        },
+        "4": {
+            "metric": "hot_water_circulation_celsius",
+            "labels": {
+                "direction": "flow"
+            }
+        },
+        "5": {
+            "metric": "solar_circulation_celsius",
+            "labels": {
+                "direction": "return"
+            }
+        },
+        "7": {
+            "metric": "solar_circulation_celsius",
+            "labels": {
+                "direction": "flow"
+            }
+        },
+        "8": {
+            "metric": "reservoir_celsius",
+            "labels": {
+                "layer": "top 2"
+            }
+        },
+        "9": {
+            "metric": "reservoir_celsius",
+            "labels": {
+                "layer": "middle 1"
+            }
+        },
+        "10": {
+            "metric": "reservoir_celsius",
+            "labels": {
+                "layer": "bottom 1"
+            }
+        },
+        "11": {
+            "metric": "boiler_circulation_celsius",
+            "labels": {
+                "direction": "flow"
+            }
+        },
+        "12": {
+            "metric": "outside_celsius"
+        },
+        "13": {
+            "metric": "heating_circulation_celsius",
+            "labels": {
+                "direction": "flow"
+            }
+        },
+        "14": {
+            "metric": "reservoir_celsius",
+            "labels": {
+                "layer": "middle 2"
+            }
+        },
+        "16": {
+            "metric": "solar_circulation_flow_rate_lperh"
+        },
+        "17": {
+            "metric": "boiler_circulation_flow_rate_lperh"
+        },
+        "18": {
+            "metric": "boiler_circulation_celsius",
+            "labels": {
+                "direction": "return"
+            }
+        },
+        "19": {
+            "metric": "solar_power_kW"
+        },
+        "20": {
+            "metric": "solar_energy_kWh_total"
+        },
+        "21": {
+            "metric": "boiler_power_kW"
+        },
+        "22": {
+            "metric": "boiler_energy_kWh_total"
+        }
+    },
+    "Logging Digital": {
+        "1": {
+            "metric": "hot_water_circulation_pump_state"
+        },
+        "2": {
+            "metric": "reservoir_charging_state",
+            "labels": {
+                "layer": "top"
+            }
+        },
+        "3": {
+            "metric": "reservoir_charging_state",
+            "labels": {
+                "layer": "middle"
+            }
+        },
+        "4": {
+            "metric": "solar_circulation_pump_state"
+        },
+        "6": {
+            "metric": "boiler_circulation_pump_state"
+        },
+        "7": {
+            "metric": "heating_circulation_pump_state"
+        },
+        "8": {
+            "metric": "heating_circulation_mixer_state"
+        },
+        "9": {
+            "metric": "heating_circulation_mixer_2_state"
+        },
+        "11": {
+            "metric": "boiler_valve_state"
+        },
+        "16": {
+            "metric": "solar_circulation_pump_2_state"
+        }
+    }
+}
+
+common_labels = {
+    'instance': 'cmi',
+    'cmi_id': cmi_id
+}
+
+
+def create_metrics(section: str, cmi_response_data, collector_registry: CollectorRegistry):
+
+    logging_section = cmi_response_data[section]
+    # print(logging_section)
+
+    existing_metrics = {}
+
+    for value in logging_section:
+        # print('value:')
+        # print(value)
+        config_key = value['Number']
+
+        if str(config_key) not in metric_mapping_config[section]:
+            continue
+
+        mapping_config = metric_mapping_config[section][str(config_key)]
+        # print(mapping_config)
+
+        labelnames = list(common_labels.keys())
+        labels = common_labels
+
+        if 'labels' in mapping_config:
+            labelnames = labelnames + list(mapping_config['labels'].keys())
+            labels = labels | mapping_config['labels']
+
+        # print(labelnames)
+        # print(labels)
+
+        metric_name = mapping_config['metric']
+        print(metric_name)
+
+        if metric_name in existing_metrics:
+            g = existing_metrics[metric_name]
+        else:
+            g = Gauge(mapping_config['metric'], 'test', namespace=metric_prefix, registry=collector_registry, labelnames=labelnames)
+            existing_metrics[metric_name] = g
+
+        g.labels(**labels).set(value['Value']['Value'])
+
+
+# http://<host>:8080/INCLUDE/api.cgi?jsonnode=1&jsonparam=Ld
+url = 'http://' + cmi_host + '/INCLUDE/api.cgi?jsonnode=1&jsonparam=La,Ld'
+response = requests.get(url, auth=(cmi_username, cmi_password))
+# print(response.text)
+
+cmi_response = response.json()
+# print(cmi_response)
+
+registry = CollectorRegistry()
+
+create_metrics('Logging Analog', cmi_response['Data'], registry)
+create_metrics('Logging Digital', cmi_response['Data'], registry)
+
+# TOOD: remove test_metric
+g = Gauge('test_metric', 'a test metric', registry=registry, labelnames=['instance', 'name'])
+# g.set(4.2)
+g.labels(instance='cmi', name='test_metric').set(4.2)
+g.labels(instance='cmi', name='test_metric2').set(3.9)
+# g.labels('name', 'test_metric')
+#g.set_to_current_time()
+
+job_name = 'test_2_cmi_extractor'
+
+push_to_gateway(pushgateway_host, job=job_name, registry=registry)
